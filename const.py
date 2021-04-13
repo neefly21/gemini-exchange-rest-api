86ac6a76188e7c77d4229092f6953130e4d5fbd9
Noah Emmons <noahe99@gmail.com>
2021-04-12 20:14:44 -0400

    Create const.py
    This file is what gemini_api_helper looks at in order to connect to the Gemini API

---
+++ const.py
@@ -0,0 +1,12 @@
+# Example with placeholders
+# Dev - Paper Money
+url = "https://api.sandbox.gemini.com"
+gemini_api_key = "account-XXXXXXXXXXXXXXXXXXXX"
+gemini_api_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXX".encode()
+discord_webhook_url = "Discord Webhook URL"
+
+# Prod - Real Money
+prod_url = "https://api.gemini.com"
+prod_gemini_api_key = "account-XXXXXXXXXXXXXXXXXXXX"
+prod_gemini_api_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXX".encode()
+prod_discord_webhook_url = "Discord Webhook URL"
